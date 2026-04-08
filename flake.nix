2e2dbc42848aa429335c42e847c69d23dfe2bc6b
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2026-04-08 11:18:11 +0200

    publish patched image

---
+++ flake.nix
@@ -0,0 +1,81 @@
+# Patched Crossplane build using Nix flakes
+{
+  description = "Patched Crossplane Docker images";
+
+  inputs = {
+    crossplane.url = "github:crossplane/crossplane/v2.2.0";
+    nixpkgs.follows = "crossplane/nixpkgs";
+    gomod2nix.follows = "crossplane/gomod2nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crossplane,
+      gomod2nix,
+    }:
+    let
+      # Reference branch/tag
+      # Only used for the OCI image tag
+      version = "v2.2.0";
+
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ gomod2nix.overlays.default ];
+      };
+
+      # Apply our patch to crossplane source
+      patchedSource = pkgs.applyPatches {
+        src = crossplane;
+        patches = [ ./crossplane.patch ];
+        name = "crossplane-patched-source";
+      };
+
+      # Reuse crossplane's build infrastructure with patched source
+      build = import "${crossplane}/nix/build.nix" {
+        inherit pkgs;
+        self = patchedSource; # Override 'self' to use patched source
+      };
+
+      apps = import "${crossplane}/nix/apps.nix" { inherit pkgs; };
+
+      imagePlatforms = [
+        {
+          os = "linux";
+          arch = "amd64";
+        }
+      ];
+
+      # Build images from patched source
+      images = build.images {
+        inherit version;
+        platforms = imagePlatforms;
+      };
+
+    in
+    {
+      packages.${system}.default = build.release {
+        inherit version;
+        goPlatforms = imagePlatforms;
+        inherit imagePlatforms;
+      };
+
+      apps.${system}.push-images = apps.pushImages {
+        inherit version images;
+        platforms = imagePlatforms;
+      };
+
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          docker-client
+          yq-go
+          kubectl
+          kubernetes-helm
+          (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+          kind
+        ];
+      };
+    };
+}
